02c4753a196bda1c7e4969aafa86631aab18bc39
konikowska <katarzyna.konikowska@gmail.com>
2018-02-23 10:32:02 +0100

    models: create cook model without foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180223092239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cooks", force: :cascade do |t|
+    t.integer "stars"
+    t.string "speciality"
+    t.string "work_city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
