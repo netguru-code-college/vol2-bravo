21a61aa4f80146e82fd90f7943712477ab75003a
konikowska <katarzyna.konikowska@gmail.com>
2018-02-23 11:57:54 +0100

    add reference between cook and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223102339) do
+ActiveRecord::Schema.define(version: 20180223104649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20180223102339) do
     t.string "work_city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_cooks_on_user_id"
   end
 
   create_table "dishes", force: :cascade do |t|
@@ -76,4 +78,5 @@ ActiveRecord::Schema.define(version: 20180223102339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cooks", "users"
 end
