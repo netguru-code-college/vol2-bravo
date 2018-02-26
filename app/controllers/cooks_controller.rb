f91e12162fc7047d1fa19332fd0195e7bd9cd573
Jacaa <mikolajczak.jaca@gmail.com>
2018-02-26 23:31:39 +0100

    Add edit, update and destroy actions of the cooks controller

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,6 +1,6 @@
 class CooksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:new, :create]
+  before_action :set_user, only: [:new, :create, :update]
   before_action :set_cook, only: [:show, :edit, :update, :destroy]
   before_action :is_cook_already?, only: [:new, :create]
 
@@ -14,7 +14,8 @@ class CooksController < ApplicationController
   def create
     @cook = Cook.new(cook_params)
     if @cook.save
-      redirect_to user_cook_path(@cook.user, @cook), notice: "Witamy nowego kucharza!"
+      notice = "Witamy nowego kucharza!"
+      redirect_to user_cook_path(@cook.user, @cook), notice: notice
     else
       render "new"
     end
@@ -24,9 +25,18 @@ class CooksController < ApplicationController
   end
 
   def update
+    if @cook.update_attributes(cook_params)
+      notice = "Profil kucharza zaktualizowany!"
+      redirect_to user_cook_path(@cook.user, @cook), notice: notice
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @cook.destroy
+    notice = "Profil kucharza został usunięty!"
+    redirect_to root_path, notice: notice
   end
 
   private
