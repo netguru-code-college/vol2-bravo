810fd61d06862a1f5ba1cdbbcb10f44e5c87729f
Jacaa <mikolajczak.jaca@gmail.com>
2018-02-26 16:07:55 +0100

    Add routes for cooks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:edit, :update, :destroy]
+  resources :users, only: [:edit, :update, :destroy] do
+    resources :cooks, only: [:show, :new, :create, :edit, :update, :destroy]
+  end
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
