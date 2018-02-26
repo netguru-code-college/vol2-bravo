1f6ea88f55e9724f1599e3c238f6b586152251f6
Jacaa <mikolajczak.jaca@gmail.com>
2018-02-26 11:25:25 +0100

    Fix associations

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,5 +1,6 @@
 class Cook < ApplicationRecord
   belongs_to :user
+  has_many   :cooks_organizations
   has_many   :organizations, through: :cooks_organizations
   has_many   :proposals,     dependent: :destroy
   has_many   :dishes,        dependent: :destroy
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,5 @@
 class Dish < ApplicationRecord
+  has_many   :dishes_ingredients
   has_many   :ingredients, through: :dishes_ingredients 
   has_many   :proposals
   belongs_to :cook
