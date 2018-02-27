138df6d8163fb7d6da8c65e0742dc671096cb4d5
Flissek <flissek@gmail.com>
2018-02-27 13:38:57 +0100

    add organization controller

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,44 @@
+class OrganizationsController < ApplicationController
+  before_action :authenticate_user!
+  #before_action :set_organization
+
+  def show
+    set_organization #@organization = Organization.find(params[:id])
+  end
+
+  def new
+    @organization = Organization.new
+  end
+
+  def create
+    @organization = Organization.new(organization_params)
+    @organization.save
+    redirect_to @organization
+  end
+
+  def edit
+    set_organization
+  end
+
+  def update
+    if @organization.update_attributes(user_params)
+      redirect_to edit_user_path(@organization), notice: "Organization updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @organization.destroy
+    redirect_to new_user_registration_path, notice: "Organization deleted!"
+  end
+
+  private
+  def organization_params
+    params.require(:organization).permit(:name, :city, :description, :private_type)
+  end
+
+  def set_organization
+    @organization = Organization.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   scope '/profile' do
     resources :users, only: [:edit, :update, :destroy] do
       resources :cooks, only: [:show, :new, :create, :edit, :update, :destroy]
+      resources :organizations, only: [:new, :create, :show, :edit, :update, :delete]
     end
   end
-  
+
   devise_for :users
 end
