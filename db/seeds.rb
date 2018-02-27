46ba9641934ef8df682796713b0f650f94ef526b
Jacaa <mikolajczak.jaca@gmail.com>
2018-02-27 21:04:19 +0100

    Update seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,85 +1,129 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: 'test@test.com',
+# Create 3 users
+User.create(email: 'user@user.com',
+            name: "Jacek",
+            last_name: "M",
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Poznań',
             interesting_city: 'Poznań',
             confirmed_at: Time.zone.now)
 
-4.times do |n|
-  User.create(email: "user-#{n}@bezinfo.com",
-              password: 'qwerty',
-              password_confirmation: 'qwerty',
-              live_city: 'Poznań',
-              interesting_city: 'Poznań',
-              confirmed_at: Time.zone.now)
-end
+User.create(email: 'cook@cook.com',
+            name: "Kasia",
+            last_name: "K",
+            password: 'qwerty',
+            password_confirmation: 'qwerty',
+            live_city: 'Poznań',
+            interesting_city: 'Poznań',
+            confirmed_at: Time.zone.now)
 
-speciality = ['polska', 'włoska', 'chińska', 'meksykańska']
+User.create(email: 'cook2@cook.com',
+            name: "Piotr",
+            last_name: "F",
+            password: 'qwerty',
+            password_confirmation: 'qwerty',
+            live_city: 'Poznań',
+            interesting_city: 'Poznań',
+            confirmed_at: Time.zone.now)
 
-users_who_want_to_be_masterchef = User.take(3)
+# Kasia and Piotr are cooks
+Cook.create(work_city: 'Poznań',
+            speciality: 'kuchnia polska',
+            user_id: 2)
 
-users_who_want_to_be_masterchef.each do |user|
-  Cook.create(work_city: 'Poznań',
-              speciality: speciality.sample,
-              user_id: user.id)
-end
+Cook.create(work_city: 'Poznań',
+            speciality: 'kuchnia polska',
+            user_id: 3)
 
-organizations = ["UAM Poznań", "Put Poznań"]
-
-Organization.create(name: "UAM Poznań",
+# Jacek creates a organization
+Organization.create(name: "PP",
                     city: 'Poznań',
                     description: 'Bardzo fajne jedzonko!',
                     private_type: true)
 
-Organization.create(name: "Politechnika Poznańska",
+Administration.create(user_id: 1, organization_id: 1)
+
+# Kasia creates a organization
+Organization.create(name: "Zupki by Kasia",
                     city: 'Poznań',
-                    description: 'Najlepsze z najlepszcych!',
+                    description: 'Najlepsze z najlepszych!',
+                    private_type: true)
+                    
+Administration.create(user_id: 2, organization_id: 2)
+
+# Piotr creates a organization
+Organization.create(name: "PiotrZur",
+                    city: 'Poznań',
+                    description: 'Żurek jak u mamy!',
                     private_type: true)
 
-Dish.create(name: 'Rosołek babci Kazi w pięciu smakach',
-            description: 'Zajebiste, polecam 10/10',
-            type_of_kitchen: 'Chińska',
-            spicyness: 1,
-            cook_id: Cook.first.id)
-
-Proposal.create(price: 12.04,
-                expiration_date: "Mon, 28 Feb 2018 12:42:53 +0100",
-                dish_id: Dish.first.id,
-                organization_id: Organization.first.id)
-
-Proposal.create(price: 20,
-                expiration_date: "Mon, 28 Feb 2018 12:42:53 +0100",
-                dish_id: Dish.first.id,
-                organization_id: Organization.second.id)
-
-
-users_who_want_to_create_organization = User.last(2)
-
+<<<<<<< HEAD
 Administration.create(user_id: users_who_want_to_create_organization[0].id,
                       organization_id: Organization.first.id)
 
 Administration.create(user_id: users_who_want_to_create_organization[1].id,
                       organization_id: Organization.second.id)
+=======
+Administration.create(user_id: 3, organization_id: 3)
 
-Organization.all.each do |organization|
-  Relationship.create(user_id: User.first.id, organization_id: organization.id)
+# Kasia is cooking in her organization and in other organization
+# Piotr is cooking only in his organization
+CooksOrganization.create(cook_id: 1, organization_id: 1)
+CooksOrganization.create(cook_id: 1, organization_id: 2)
+CooksOrganization.create(cook_id: 2, organization_id: 3)
+>>>>>>> Update seed.db
+
+# Kasia creates a new dish
+Dish.create(name: 'Rosołek babci Kazi w pięciu smakach',
+            description: 'Zajebiste, polecam 10/10',
+            type_of_kitchen: 'kuchnia polska',
+            cook_id: 1)
+
+Dish.create(name: 'Rosołek z internetu',
+            description: 'Bardzo fajna zupka',
+            type_of_kitchen: 'kuchnia polska',
+            cook_id: 1)
+
+# Piotr creates a new dish
+Dish.create(name: 'Żurek',
+            description: 'Najlepszy żurek w Poznaniu',
+            type_of_kitchen: 'kuchnia polska',
+            cook_id: 2)
+
+# Kasia creates a new proposal in her organization
+Proposal.create(price: 3.00,
+                quantity: 1000,
+                expiration_date: "Mon, 28 Feb 2018 13:00:00 +0100",
+                dish_id: 1,
+                organization_id: 2)
+
+# Kasia creates a new proposal in other organization
+Proposal.create(price: 2.00,
+                quantity: 600,
+                expiration_date: "Mon, 28 Feb 2018 17:00:00 +0100",
+                dish_id: 2,
+                organization_id: 1)
+
+# Piotr creates a new proposal in his organization
+Proposal.create(price: 4.00,
+                quantity: 700,
+                expiration_date: "Mon, 28 Feb 2018 15:00:00 +0100",
+                dish_id: 3,
+                organization_id: 3)
+
+# Jacek follows all organizations
+3.times do |n| 
+  Relationship.create(user_id: 1, organization_id: n+1)
 end
 
-Ingredient.create(
-            name: 'Marchewka',
-            kind: 0)
+# Kasia follows Piotr's organization
+Relationship.create(user_id: 2, organization_id: 3)
 
-Ingredient.create(
-            name: 'Kostka rosołowa',
-            kind: 1)
+# Create some ingredients
+Ingredient.create(name: 'Marchewka', kind: 'Warzywo')
 
-Ingredient.all.each do |ingredient|
-  DishesIngredient.create(dish_id: Dish.first.id, ingredient_id: ingredient.id)
-end
+Ingredient.create(name: 'Kostka rosołowa', kind: 'Inne')
+
+DishesIngredient.create(dish_id: 1, ingredient_id: 1)
+
+# Create custom orders
