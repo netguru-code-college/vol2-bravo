92ba106af4a4977712f705687198957339f769ed
Jacaa <mikolajczak.jaca@gmail.com>
2018-02-28 23:02:25 +0100

    Fix heroku migrations

--- db/migrate/20180228220053_add_conf_columns_to_users.rb
+++ db/migrate/20180228220053_add_conf_columns_to_users.rb
@@ -1,4 +1,4 @@
-class AddConfirmationColumnsToUser < ActiveRecord::Migration[5.1]
+class AddConfColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
