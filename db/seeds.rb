7e7b8c576291e6f937acec7274dbd89665dda74d
Flissek <flissek@gmail.com>
2018-03-01 09:40:06 +0000

    fix names in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # Create 10 users
 User.create(email: 'user1@user.com',
             name: 'Jacek',
-            last_name: 'M',
+            last_name: 'Minaret',
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Poznań',
@@ -10,7 +10,7 @@ User.create(email: 'user1@user.com',
 
 User.create(email: 'user2@user.com',
             name: 'Kasia',
-            last_name: 'K',
+            last_name: 'Król',
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Poznań',
@@ -19,7 +19,7 @@ User.create(email: 'user2@user.com',
 
 User.create(email: 'user3@user.com',
             name: 'Piotr',
-            last_name: 'F',
+            last_name: 'Fengshui',
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Kraków',
@@ -28,7 +28,7 @@ User.create(email: 'user3@user.com',
             
 User.create(email: 'user4@user.com',
             name: 'Zuzanna',
-            last_name: 'S',
+            last_name: 'Sowińska',
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Kraków',
@@ -37,7 +37,7 @@ User.create(email: 'user4@user.com',
             
 User.create(email: 'user5@user.com',
             name: 'Robert',
-            last_name: 'K',
+            last_name: 'Kowalski',
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Warszawa',
@@ -46,7 +46,7 @@ User.create(email: 'user5@user.com',
             
 User.create(email: 'user6@user.com',
             name: 'Łukasz',
-            last_name: 'O',
+            last_name: 'Oliwa',
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Katowice',
@@ -55,7 +55,7 @@ User.create(email: 'user6@user.com',
             
 User.create(email: 'user7@user.com',
             name: 'Darek',
-            last_name: 'M',
+            last_name: 'Marczak',
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Poznań',
@@ -64,7 +64,7 @@ User.create(email: 'user7@user.com',
 
 User.create(email: 'user8@user.com',
             name: "Lidia",
-            last_name: "W",
+            last_name: "Winiary",
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Warszawa',
@@ -73,7 +73,7 @@ User.create(email: 'user8@user.com',
             
 User.create(email: 'user9@user.com',
             name: "Patryk",
-            last_name: "S",
+            last_name: "Sandbag",
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Katowice',
@@ -82,26 +82,30 @@ User.create(email: 'user9@user.com',
             
 User.create(email: 'user10@user.com',
             name: "Bartosz",
-            last_name: "S",
+            last_name: "Skowronek",
             password: 'qwerty',
             password_confirmation: 'qwerty',
             live_city: 'Poznań',
             interesting_city: 'Poznań',
             confirmed_at: Time.zone.now)
 
-# Kasia and Piotr are cooks
+# Kasia and Jacek are cooks
 Cook.create(work_city: 'Poznań',
             speciality: 'kuchnia polska',
             user_id: 2)
 
+Cook.create(work_city: 'Kraków',
+            speciality: 'kuchnia studencka',
+            user_id: 1)
+
 Cook.create(work_city: 'Poznań',
             speciality: 'kuchnia polska',
-            user_id: 3)
+            user_id: 7)
 
 # Jacek creates a organization
-Organization.create(name: "PP",
+Organization.create(name: "Akademik Góral",
                     city: 'Poznań',
-                    description: 'Bardzo fajne jedzonko!',
+                    description: 'Zjedz pysznie, tanio i zdrowo prosto od studentów!',
                     private_type: true)
 
 Administration.create(user_id: 1, organization_id: 1)
@@ -109,14 +113,14 @@ Administration.create(user_id: 1, organization_id: 1)
 # Kasia creates a organization
 Organization.create(name: "Zupki by Kasia",
                     city: 'Poznań',
-                    description: 'Najlepsze z najlepszych!',
+                    description: 'Najlepsze z najlepszych! Na bazie zupek Knorr.',
                     private_type: true)
 
 Administration.create(user_id: 2, organization_id: 2)
 
 # Piotr creates a organization
-Organization.create(name: "PiotrZur",
-                    city: 'Poznań',
+Organization.create(name: "BlaBlaZur",
+                    city: 'Kraków',
                     description: 'Żurek jak u mamy!',
                     private_type: true)
 
@@ -148,7 +152,7 @@ Dish.create(name: 'Żurek',
 # Kasia creates a new proposal in her organization
 Proposal.create(price: 0.00,
                 quantity: 2,
-                expiration_date: "Mon, 28 Feb 2018 13:00:00 +0100",
+                expiration_date: "Mon, 01 Mar 2018 18:00:00 +0100",
                 dish_id: 1,
                 organization_id: 2,
                 cook_id: 1)
@@ -156,7 +160,7 @@ Proposal.create(price: 0.00,
 # Kasia creates a new proposal in other organization
 Proposal.create(price: 2.00,
                 quantity: 3,
-                expiration_date: "Mon, 28 Feb 2018 17:00:00 +0100",
+                expiration_date: "Mon, 02 Mar 2018 21:00:00 +0100",
                 dish_id: 2,
                 organization_id: 1,
                 cook_id: 1)
@@ -164,7 +168,7 @@ Proposal.create(price: 2.00,
 # Piotr creates a new proposal in his organization
 Proposal.create(price: 4.00,
                 quantity: 10,
-                expiration_date: "Mon, 28 Feb 2018 15:00:00 +0100",
+                expiration_date: "Mon, 03 Mar 2018 15:00:00 +0100",
                 dish_id: 3,
                 organization_id: 3,
                 cook_id: 2)
