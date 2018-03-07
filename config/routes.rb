6d3cd47fb6176df297fe52620565203142af2c95
Jacaa <mikolajczak.jaca@gmail.com>
2018-03-07 01:11:26 +0100

    Fix 1st issue - add I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
 
   root 'dashboard#index'
+  
+  get '/:locale', to: 'dashboard#index'
 
-  scope '/profile' do
-    resources :users,   only: [:edit, :update, :destroy] do
-      resources :cooks, only: [:show, :new, :create, :edit, :update, :destroy]
+  scope '(:locale)', locale: /en|pl/ do
+    scope '/profile' do
+      resources :users,   only: [:edit, :update, :destroy] do
+        resources :cooks, only: [:show, :new, :create, :edit, :update, :destroy]
+      end
+      resources :organizations, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+        resources :proposals,   only: [:new, :create, :show, :edit, :update, :destroy, :index]
+      end
+      get  '/custom_orders/:proposal_id/new', to: 'custom_orders#new',    as: 'custom_order'
+      post '/custom_orders/:proposal_id',     to: 'custom_orders#create', as: 'custom_orders'
     end
-    resources :organizations, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-      resources :proposals,   only: [:new, :create, :show, :edit, :update, :destroy, :index]
-    end
-    get  '/custom_orders/:proposal_id/new', to: 'custom_orders#new',    as: 'custom_order'
-    post '/custom_orders/:proposal_id',     to: 'custom_orders#create', as: 'custom_orders'
   end
 
   devise_for :users
